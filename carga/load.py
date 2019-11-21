3ea9c089b3e03ba1ca30aa332d302250847500a6
Leonardo Croda <lccroda@gmail.com>
2019-11-20 22:04:42 -0300

    inserindo no dw

--- carga/load.py
+++ carga/load.py
@@ -15,20 +15,20 @@ def inserir(dataframe):
     for texto in textos:
         id=ids[i]
         sql = "insert into dimtext(iddimtext,text) values (" + str(id) + ','+ "$string$"+ str(texto) + "$string$)"
-        print(sql)
-        print(i)
-    #     cur.execute(sql)
+        # print(sql)
+        # print(i)
+        cur.execute(sql)
         i=i+1
-    # i=0
-    # for data in tempos:
-    #     id=ids[i]
-    #     sql = "insert into dimtime(iddimtime,day,month,year,dayweek,hour,shift) values("+ str(id)+","+ str(data['day'])+",'"+data['month']+"',"+str(data['year'])+",'"+data['dayweek']+"','"+data['hour']+"','"+data['shift']+"')"
-    #     cur.execute(sql)
-    #     i=i+1
-    # for tweet in dataframe.itertuples(index = True, name='Pandas'):
-    #     # print(tweet)
-    #     sql= "insert into facttweet(characteristics,feeling,text,time) values ('"+getattr(tweet,'pilares')+"',"+str(getattr(tweet,'sentimento'))+","+str(getattr(tweet,"id"))+","+str(getattr(tweet,"id"))+")"
-    #     cur.execute(sql)
-    # con.commit()
-    # con.close()
+    i=0
+    for data in tempos:
+        id=ids[i]
+        sql = "insert into dimtime(iddimtime,day,month,year,dayweek,hour,shift) values("+ str(id)+","+ str(data['day'])+",'"+data['month']+"',"+str(data['year'])+",'"+data['dayweek']+"','"+data['hour']+"','"+data['shift']+"')"
+        cur.execute(sql)
+        i=i+1
+    for tweet in dataframe.itertuples(index = True, name='Pandas'):
+        # print(tweet)
+        sql= "insert into facttweet(characteristics,feeling,text,time) values ('"+getattr(tweet,'pilares')+"',"+str(getattr(tweet,'sentimento'))+","+str(getattr(tweet,"id"))+","+str(getattr(tweet,"id"))+")"
+        cur.execute(sql)
+    con.commit()
+    con.close()
 
